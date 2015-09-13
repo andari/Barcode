b7a46f91e55a95c0765e1b2289a6110603f76e88
Andreas Aristotelous <aaristot@gmail.com>
2015-09-13 22:00:28 +0200

    website construction

--- barcode/main.c
+++ barcode/main.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <signal.h>
 #include <stropts.h>
+#include <curl/curl.h>
 
 // custom libraries
 #include "barcodeParser.h"
@@ -29,6 +30,13 @@ void catchSignal(int signal){
 }
 
 int main(int argc, char** argv){
+	CURL *curl;
+	CURLcode res;
+	
+	curl_global_init(CURL_GLOBAL_ALL);
+
+	curl = curl_easy_init();
+
 	// check if enough arguments are passed
 	if (argc < 2){
 		printf("Not enough arguments\n");
@@ -82,18 +90,43 @@ int main(int argc, char** argv){
 					bzero(buffer, sizeof(buffer));
 				}
 			}
-				
+							
 			if (parseBarcodeContext(context, &output) == SUCCESS){
+				if(curl){
+				char *datasent = output.line;
+				//const char *datasent = "Hello World!!!!!!!!!!!!!!!!!!!1!";
+				//printf("the code lies here and datasent is %s\n", datasent);
+				//curl_easy_setopt(curl, CURLOPT_POSTFIELDS, datasent);	
+				//printf("the code lies here after curl to outputline\n");
+				curl_easy_setopt(curl, CURLOPT_URL, "http://169.254.164.6:8000/licence/");  //169.254.164.6
+				curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, 20L);
+				//curl_easy_setopt(curl, CURLOPT_POSTFIELDS, datasent);
+				curl_easy_setopt(curl, CURLOPT_COPYPOSTFIELDS, datasent);
+					
 				//printf("BARCODE SCAN SUCCESS \n");
-				printf("BARCODE RESULT : %s\n", output.line);
+				//printf("BARCODE RESULT : %s\n", output.line);
+				//curl_easy_setopt(curl, CURLOPT_POSTFIELDS, datasent);
+				//context = initializeBarcodeContext();
+				//bzero(&output,sizeof(output)) ;
+				res = curl_easy_perform(curl);
+				if(res != CURLE_OK){
+				 fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+				}
+				//curl_easy_cleanup(curl);
+
+				//printf("BARCODE SCAN SUCCESS \n");
+				//printf("BARCODE RESULT : %s\n", output.line);
 				context = initializeBarcodeContext();
 				bzero(&output,sizeof(output)) ;
+				curl_easy_cleanup(curl);
+ 
 			}
 		}
 		
 		// zero it out so we can reuse the structure
 		fds[0].revents = 0;
-	}
-
+		}
+	}	
+	//curl_global_cleanup();
 	exit(EXIT_SUCCESS);
 }
